3e4db1b680d0a2af036a647a4924ea9b7a514d6c
Electimore <61228588+Electimore@users.noreply.github.com>
2021-02-28 22:41:25 +0100

    🥅Eliminated errors when downloading too many images

--- index.js
+++ index.js
@@ -18,20 +18,33 @@ fs.readdir(dir, (err, files) => {
 getImages = async (tags, count) => {
   tags = tags.concat(" rating:safe -presenting -topless -meme -text -<3 -spooning -pregnant -vore -octopus -naked -bulge -poppy's69 -diaper ") //Advanced safe-search. Don't even ask. We hurt our eyes so you don't have to. 90% effective-ish maybe.
   let images = await e621.postsList({tags, limit: count})
+  let errors = 0;
+  const downloads = [];
+  if(count > 200){
+    console.log(`${count} is bigger than 200! try again!`)
+    return;
+  }
   for (let i = 0; i < count; i++) {
-    if(images.data.posts[i].file.url){
-      download.image({url: images.data.posts[i].file.url, dest: "./images"});
+    if(images.data.posts[i] && images.data.posts[i].file.url){
+      downloads.push(download.image({url: images.data.posts[i].file.url, dest: "./images"}));
+    }
+    else{
+      console.log(`Image number ${i+1} could not be downloaded.`)
+      errors += 1;
     }
   }
+  await Promise.all(downloads);
+  console.log(`Done! Downloaded ${count-errors} images. ${errors ? errors + " images could not be downloaded.":""}`)
+  return;
 }
 
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-});
+}); 
    
 readline.question('Input your tags: ', query => {
-    readline.question('How many pictures would you like? ', number => {
+    readline.question('How many pictures would you like (200 or less)? ', number => {
         console.log(`${number} images with tags: ${query}\nare downloading!`);
     getImages(query, number);
     readline.close();
