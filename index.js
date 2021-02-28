251c3830ce7cc39fcb096bb9a1e4cc0f402edc5f
Electimore <61228588+Electimore@users.noreply.github.com>
2021-02-28 01:44:17 +0100

    ✨Added input system for tags and image count

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const e621 = new (require(`e621-api-wrapper`)).e621("auto-stash", {base_url: "https://e926.net" });
 const download = require(`image-downloader`)
+const fs = require('fs');
+const path = require('path');
 
-getImages = async (tags) => {
-  tags = tags.concat(" rating:safe")
-  console.log(tags)
+const dir = "images";
+
+fs.readdir(dir, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(dir, file), err => {
+      if (err) throw err;
+    });
+  }
+});
+
+getImages = async (tags, count) => {
+  tags = tags.concat(" rating:safe -presenting -topless -meme -text -<3 -spooning -pregnant -vore -octopus -naked -bulge -poppy's69") //Advanced safe-search. Don't even ask. We hurt our eyes so you don't have to. 90% effective-ish maybe.
   let images = await e621.postsList({tags})
 
-  console.log(images.data.posts)
-
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < count; i++) {
     download.image({url: images.data.posts[i].file.url, dest: "./images"})
   }
 }
-getImages("wolf")
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+   
+readline.question('Input your tags: ', query => {
+    readline.question('How many pictures would you like? ', number => {
+        console.log(`${number} images with tags: ${query}\nare downloading!`);
+    getImages(query, number);
+    readline.close();
+    });
+});
