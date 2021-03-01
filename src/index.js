5163b230893954b035665786913aeb37853917db
luke2k04 <luke2k04@gmail.com>
2021-03-01 20:11:08 +0100

    :boom: Made deviantart work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-const e621 = new (require(`e621-api-wrapper`)).e621("auto-stash", {base_url: "https://e926.net" })
 const download = require(`image-downloader`)
 const fs = require('fs')
 const path = require('path')
@@ -7,8 +6,8 @@ const deviantart = require('deviantart.ts')
 
 async function getAPI() {
   const deviantApp = await deviantart.default.login(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
-  const deviation = await deviantApp.rss.get('https://www.deviantart.com/akreon/art/Yordles-868586083')
-  console.log(deviation.content[0].url)
+  const deviation = await deviantApp.rss.search("furry wolf", 1, "popular")
+  console.log(deviation[0].content[0].url)
 }
 
 getAPI()
