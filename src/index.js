9de8acadcd54c12a492c6b6ca6fcc225868933f5
luke2k04 <luke2k04@gmail.com>
2021-03-01 21:10:33 +0100

    Revert ":boom: Added sysargs"
    This reverts commit 561f8805ac24fa1e1ad607d400197e196f810df9.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const fs = require('fs')
 const path = require('path')
 const deviantart = require('deviantart.ts')
 
-// async function getAPI() {
-//   const deviantApp = await deviantart.default.login(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
-//   const deviation = await deviantApp.rss.search("furry wolf", 1, "popular")
-//   console.log(deviation[0].content[0].url)
-// }
+async function getAPI() {
+  const deviantApp = await deviantart.default.login(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
+  const deviation = await deviantApp.rss.search("furry wolf", 1, "popular")
+  console.log(deviation[0].content[0].url)
+}
 
-// getAPI()
+getAPI()
