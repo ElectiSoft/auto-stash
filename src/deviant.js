7a7498e0a0f4029a8ca2baa3573d481aa5082c72
Electimore <61228588+Electimore@users.noreply.github.com>
2021-03-02 14:02:02 +0100

    :sparkles: Added file deleting

--- src/deviant.js
+++ src/deviant.js
@@ -5,6 +5,17 @@ const path = require('path')
 const deviantart = require('deviantart.ts')
 const { count } = require('console')
 
+function clearDir(dir){
+  fs.readdir(dir, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      fs.unlink(path.join(dir, file), err => {
+        if (err) throw err;
+      });
+    }
+  });
+}
+
 function getAPI() {
   return deviantart.default.login(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
 }
@@ -21,9 +32,12 @@ async function downloadImages(searchResults, dir){
   await Promise.all(downloads)
 }
 
-function downloadDeviantart(query, count, sort, dir){
+function downloadDeviantart({search, count, sort, dir, clear}){
+  if(clear){
+    clearDir(dir)
+  }
   getAPI()
-  .then(api => searchImages(api, query, count, sort)
+  .then(api => searchImages(api, search, count, sort)
   .then(results => downloadImages(results, dir)))
 }
 
